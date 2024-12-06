5db36be1093462f81abd4898ed4a679a293467d0
Asim Rauf <idmery77@gmail.com>
2024-12-06 23:50:54 +0500

    Update environment variable names

--- config/aws.ts
+++ config/aws.ts
@@ -1,7 +1,7 @@
 export const awsConfig = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  region: process.env.AWS_REGION!,
-  bucketName: process.env.AWS_BUCKET_NAME!,
+  accessKeyId: process.env.ACCESS_KEY_ID!,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+  region: process.env.REGION!,
+  bucketName: process.env.BUCKET_NAME!,
   cloudFrontUrl: process.env.CLOUDFRONT_URL!
 };

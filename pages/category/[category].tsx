fa46d6f1a571ac2f0217b11f6f55a2832b1269c9
Asim Rauf <idmery77@gmail.com>
2024-12-07 00:15:32 +0500

    Fix module resolution and import paths

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import ProductCard from '@/components/ProductCard'
+import ProductCard from '../../components/ProductCard'
 import { FiFilter, FiChevronDown } from 'react-icons/fi'
 
 export default function CategoryProducts() {

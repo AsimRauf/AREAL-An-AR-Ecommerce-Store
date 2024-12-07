310f48844336037dd31eb5e3735a5aa99fde7b74
unknown <Asim Rauf>
2024-12-07 14:45:48 +0500

    feat: add environment variables configuration

--- pages/api/env-check.ts
+++ pages/api/env-check.ts
@@ -1,6 +1,9 @@
 export default function handler(req, res) {
   res.json({
-    has_password: !!process.env.MONGODB_PASSWORD,
-    password_length: process.env.MONGODB_PASSWORD?.length || 0
+    hasAccessKey: !!process.env.ACCESS_KEY_ID,
+    hasSecretKey: !!process.env.SECRET_ACCESS_KEY,
+    hasRegion: !!process.env.REGION,
+    hasBucketName: !!process.env.BUCKET_NAME,
+    hasMongoDbUri: !!process.env.MONGODB_URI,
   })
 }

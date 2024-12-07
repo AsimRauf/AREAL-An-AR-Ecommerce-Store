dfd671b37e334da888101587b026d51d5b7e1a02
Asim Rauf <idmery77@gmail.com>
2024-12-07 21:56:15 +0500

    fix: update products endpoint with signed URLs

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -2,12 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { connectDB } from '../../config/db'
 import { Product } from '../../models/Product'
 import { Seller } from '../../models/Seller'
-import { generateSignedUrl } from '../../utils/cloudfront'
+import { getEnvironmentSignedUrl } from '../../utils/url-generator'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' })
   }
@@ -15,7 +16,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     await connectDB()
     
-    // Fetch products with essential fields
     const products = await Product.find()
       .populate({
         path: 'seller',
@@ -31,12 +31,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const productsWithSignedUrls = await Promise.all(products.map(async product => {
       // Process product images
       const productImages = await Promise.all(
-        product.images.slice(0, 1).map(key => generateSignedUrl(key))
+        product.images.slice(0, 1).map(key => getEnvironmentSignedUrl(key))
       )
 
       // Process seller image if exists
-      const sellerImage = product.seller?.profileImage ? 
-        await generateSignedUrl(product.seller.profileImage) : null
+      const sellerImage = product.seller?.profileImage ?
+        await getEnvironmentSignedUrl(product.seller.profileImage) : null
 
       return {
         ...product,

6aec234f5898eca40f539b1538719197c79dc2d3
unknown <Asim Rauf>
2024-12-07 16:17:54 +0500

    Fix signed URL for amplify

--- pages/api/debug-cloudfront.ts
+++ pages/api/debug-cloudfront.ts
@@ -1,18 +1,21 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { generateSignedUrl } from '../../utils/cloudfront'
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const testKey = 'test-image.jpg';
+    // Use an actual image key from your database
+    const testKey = 'products/674810230805b28a42056803/test.jpg';
+    
+    // Log the private key format
+    const privateKey = process.env.CLOUDFRONT_PRIVATE_KEY!
+      .replace(/\\n/g, '\n')
+      .replace(/"([^"]+)"/, '$1');
+    
+    console.log('Private key first line:', privateKey.split('\n')[1]);
   
-  const result = {
-    originalKey: testKey,
-    signedUrl: await generateSignedUrl(testKey),
-    env: {
-      hasCloudFrontUrl: !!process.env.CLOUDFRONT_URL,
-      hasKeyPairId: !!process.env.CLOUDFRONT_KEY_PAIR_ID,
-      hasPrivateKey: !!process.env.CLOUDFRONT_PRIVATE_KEY?.length
-    }
-  };
-  
-  res.status(200).json(result);
-}
+    const result = {
+      originalKey: testKey,
+      signedUrl: await generateSignedUrl(testKey),
+      cloudFrontUrl: process.env.CLOUDFRONT_URL,
+      keyPairIdPrefix: process.env.CLOUDFRONT_KEY_PAIR_ID?.substring(0, 4),
+      privateKeyLength: privateKey.length
+    };
+    
+    res.status(200).json(result);
+  }

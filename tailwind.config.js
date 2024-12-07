d8e6cea996774e9b3cd336aaf55287a001398f46
Asim Rauf <idmery77@gmail.com>
2024-12-07 14:22:45 +0500

    fix: update Amplify build configuration for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
 }
